8217c63bdc6eaad558de4b0ef40d2fd7515b9ced
Galo Lopez <galolopez18@gmail.com>
2019-01-20 22:36:38 -0500

    Blog project upload

---
+++ Galo_Personal_Blog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Galo_Personal_Blog.MvcApplication" Language="C#" %>
---
+++ Galo_Personal_Blog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Galo_Personal_Blog.Startup))]
+namespace Galo_Personal_Blog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
